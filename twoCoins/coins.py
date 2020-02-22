38d8314ebb842c287757d1ce73dd0e8c75153e98
Evan Moses <evanfcmoses@gmail.com>
2020-02-22 11:34:54 -0600

    starting two coins test

---
+++ twoCoins/coins.py
@@ -0,0 +1,19 @@
+
+class Coin:
+    
+    results = []
+    
+    iterationAmount
+
+    def __init__(self, flipAmounts):
+        self.iterationAmount = flipAmounts
+
+    def conservativeWhenDoingWell():
+
+
+
+class OnePointCoin(Coin):
+    
+    
+    
+    def
